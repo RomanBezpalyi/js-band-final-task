7123861d928f1918709f1e950cee8d2da0b0ff4f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-06 16:07:42 +0000

    Deploy RomanBezpalyi/js-band-final-task to github.com/RomanBezpalyi/js-band-final-task.git:gh-pages

---
+++ precache-manifest.e902d7319a4e0f5e8f7b811eacfaba50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94eea1d6367e6654fc0a6997dabda250",
+    "url": "/js-band-final-task/index.html"
+  },
+  {
+    "revision": "a0ba08af7fff55f5c3e9",
+    "url": "/js-band-final-task/static/css/2.1b28e48e.chunk.css"
+  },
+  {
+    "revision": "3a94026d8317418d2181",
+    "url": "/js-band-final-task/static/css/main.3a75610e.chunk.css"
+  },
+  {
+    "revision": "a0ba08af7fff55f5c3e9",
+    "url": "/js-band-final-task/static/js/2.ad0e9f70.chunk.js"
+  },
+  {
+    "revision": "3a94026d8317418d2181",
+    "url": "/js-band-final-task/static/js/main.7469caf4.chunk.js"
+  },
+  {
+    "revision": "49af006cc0cf04963d6b",
+    "url": "/js-band-final-task/static/js/runtime-main.3be8268d.js"
+  },
+  {
+    "revision": "0aee39a996168344b3da0916f4c732db",
+    "url": "/js-band-final-task/static/media/check-mark.0aee39a9.svg"
+  }
+]);
